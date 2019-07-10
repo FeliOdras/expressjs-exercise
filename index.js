dd5ad99beaa17225468c8f6e12b7c0bbe296eec4
FeliOdras <ariane.feldmann@odras.de>
2019-07-10 11:21:55 +0200

    Implemented myLogger middleware

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ const myLogger = (req, res, next) => {
   next();
 };
 
+app.use(myLogger);
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
